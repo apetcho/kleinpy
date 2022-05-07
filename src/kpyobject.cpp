2ff5c96fb0c4e1f8536f7c0cdc6f4a2bd9d60e25
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-07 13:39:33 +0200

    Implement  kleinpy::KPyObject::call_method() method

--- src/kpyobject.cpp
+++ src/kpyobject.cpp
@@ -1,8 +1,27 @@
+#include<functional>
 #include "kleinpy.hpp"
+
 using namespace kleinpy;
 // ---
 std::ostream& operator<<(
     std::ostream& strm, KPyObject kpyObj){
     strm << kpyObj.to_string();
     return strm;
+}
+
+// ----
+KPyObject* KPyObject::call_method(
+    std::string name, std::vector<KPyObject*>* args
+){
+    KPyObject* (KPyObject::*fun)(std::vector<KPyObject*>*);
+    if(dict.find(name) == dict.end()){
+        throw new KPyException(
+            KPYILLEGALOPERATIONEXCEPTION,
+            "TypeError: '" + get_type()->to_string() + 
+            "' object has no attribute '" + name + "'"
+        );
+    }
+    fun = dict[name];
+
+    return (this->*fun)(args);
 }
