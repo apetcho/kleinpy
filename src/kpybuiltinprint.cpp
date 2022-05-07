4a5b9bd20bc8cb059a11c2178d7abbec43b63d01
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-07 15:44:08 +0200

    Implement  kleinpy::KPyBuiltinPrint::KPyBuiltinPrint() default constructor

---
+++ src/kpybuiltinprint.cpp
@@ -0,0 +1,41 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+// ---
+KPyBuiltinPrint::KPyBuiltinPrint() : KPyCallable(){}
+
+// // ---
+// KPyBuiltinIter::KPyBuiltinIter(const KPyBuiltinIter& other){}
+
+// // ---
+// KPyBuiltinIter::~KPyBuiltinIter(){}
+
+
+// // ---
+// KPyType* KPyBuiltinIter::get_type(){
+//     return kpytypes[KPyTypeId::KPY_BUILTIN_TYPE];
+// }
+
+// // ---
+// KPyObject* KPyBuiltinIter::__call__(std::vector<KPyObject*>* args){
+//     std::vector<KPyObject*>* iterargs = new std::vector<KPyObject*>();
+//     KPyObject *obj;
+//     std::ostringstream oss;
+
+//     if(args->size() != 1){
+//         oss << "TypeError: expected 1 argument, got " << args->size();
+//         throw new KPyException(KPYWRONGARGCOUNTEXCEPTION, oss.str());
+//     }
+
+//     obj = (*args)[0];
+//     KPyObject *result = obj->call_method("__iter__", iterargs);
+
+//     return result;
+// }
+
+
+// // ---
+// std::string KPyBuiltinIter::to_string(){
+//     return "iter";
+// }
