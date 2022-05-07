d4a2d0f46f1d3a4e1b68254f2c82530bcd819c2b
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-07 14:44:10 +0200

    Implement  kleinpy::KPyBuiltinConcat::__call__() destructor

--- src/kpybuiltinconcat.cpp
+++ src/kpybuiltinconcat.cpp
@@ -10,3 +10,18 @@ KPyBuiltinConcat::KPyBuiltinConcat(const KPyBuiltinConcat& obj){}
 
 // ---
 KPyBuiltinConcat::~KPyBuiltinConcat(){}
+
+// ---
+KPyObject* KPyBuiltinConcat::__call__(std::vector<KPyObject*>* args){
+    std::ostringstream oss;
+    if(args->size() != 1){
+        oss << "TypeError: expected 1 argument, got " << args->size();
+        throw new KPyException(KPYWRONGARGCOUNTEXCEPTION, oss.str());
+    }
+
+    KPyObject *target;
+    target = (*args)[0];
+    std::vector<KPyObject*>* callargs = new std::vector<KPyObject*>();
+
+    target->call_method("concat", callargs);
+}
