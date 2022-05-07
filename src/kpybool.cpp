1897a02a4fd3508ac42d1ced9e124dd4417b059b
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-07 14:16:36 +0200

    Implement  kleinpy::KPyBool::KPyBool() default constructor

---
+++ src/kpybool.cpp
@@ -0,0 +1,19 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+KPyBool::KPyBool() : KPyObject(){
+    value = false;
+    dict["__float__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyBool::__float__
+    );
+    dict["__int__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyBool::__int__
+    );
+    dict["__bool__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyBool::__bool__
+    );
+    dict["__eq__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyBool::__eq__
+    );
+}
