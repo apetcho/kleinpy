8d7e21f49407b094560c4edfddb4419c530a675b
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-08 19:49:16 +0200

    Implement kleinpy::KPyExceptionType::__call__() method

--- src/kpyexceptiontype.cpp
+++ src/kpyexceptiontype.cpp
@@ -8,3 +8,28 @@ KPyExceptionType::KPyExceptionType(std::string typestr, KPyTypeId id)
 
 // ---
 KPyExceptionType::~KPyExceptionType(){}
+
+// ---
+KPyObject* KPyExceptionType::__call__(std::vector<KPyObject*>* args){
+    std::ostringstream oss;
+    int exctype;
+
+    if(args->size() > 2){
+        oss << "TypeError: excepted 1 or 2 arguments, got " << args->size();
+        throw new KPyExceptionType(KPYWRONGARGCOUNTEXCEPTION, oss.str());
+    }
+
+    if(args->size() == 1){
+        return new KPyException(KPYEXCEPTION, (*args)[0]);
+    }
+
+    if((*args)[0]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+        throw new KPyException(
+            KPYILLEGALOPERATIONEXCEPTION,
+            "TypeError: excepted in for first argument ot Exception."
+        );
+    }
+    exctype = ((KPyInt*)(*args)[0])->get_value();
+
+    return new KPyException(exctype, (*args)[1]);
+}
