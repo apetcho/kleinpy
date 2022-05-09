c161e2b6ef8d1b4990bfa448d7036e5c3bc3e863
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-10 01:12:49 +0200

    Implement kleinpy::KPyInt::KPyInt() constructor

---
+++ src/kpyint.cpp
@@ -0,0 +1,47 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+KPyInt::KPyInt(int value) : KPyObject() {
+    this->value = value;
+    dict["__add__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__add__
+    );
+    dict["__sub__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__sub__
+    );
+    dict["__mul__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__mul__
+    );
+    dict["__floordiv__"]=(KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__floordiv__
+    );
+    dict["__truediv__"]=(KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__truediv__
+    );
+    dict["__eq__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__eq__
+    );
+    dict["__gt__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__gt__
+    );
+    dict["__lt__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__lt__
+    );
+    dict["__ge__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__ge__
+    );
+    dict["__le__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__le__
+    );
+    dict["__float__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__float__
+    );
+    dict["__int__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__int__
+    );
+    dict["__bool__"] = (KPyObject* (KPyObject::*)(std::vectory<KPyObject*>*))(
+        &KPyInt::__bool__
+    );
+
+}
