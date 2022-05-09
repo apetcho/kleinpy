45c727956d0c9d12e46c4dc08ad8c39909be09a3
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-09 10:44:27 +0200

    Implement kleinpy::KPyFrame::KPyFrame() constructor

--- src/kpyframe.cpp
+++ src/kpyframe.cpp
@@ -16,3 +16,34 @@ const char* richcomp[13] = {
     "__excmatch__",
     "BAD"
 };
+
+// ---
+KPyFrame::KPyFrame(
+    const KPyCode& _code,
+    std::vector<KPyObject*>* args,
+    std::unordered_map<std::string, KPyObject*>& _globals,
+    std::vector<KPyObject*>& _constants,
+    std::unordered_map<std::string, KPyCell*>& _cellVars
+)
+: code(_code), globals(_globals),
+ consts(_constants), cellVars(_cellVars)
+{
+    std::vector<std::string>& vnames = code.get_locals();
+    this->opstack = new KPyStack<KPyObject*>();
+    this->blockstack = new KPyStack<int>();
+
+    int n = args->size() - 1;
+    for(int i=0; i < args->size(); i++){
+        locals[vnames[i]] = (*args)[n--];
+    }
+
+    //
+    for(int i=0; this->code.get_cellVars().size(); i++){
+        std::string name = code.get_cellVars()[i];
+        cellVars[name] = new KPyCell(NULL);
+
+        if(locals.find(name) != locals.end()){
+            cellVars[name]->set(locals[name]);
+        }
+    }
+}
