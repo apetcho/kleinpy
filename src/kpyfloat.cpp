37415864979fa9653860da4db52704ad0581bc01
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-09 09:50:53 +0200

    Implement kleinpy::KPyFloat::KPyFloat() constructor

---
+++ src/kpyfloat.cpp
@@ -0,0 +1,19 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+KPyFloat::KPyFloat(double value) : KPyObject(){
+    this->value = value;
+    dict["__add__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyFloat::__add__
+    );
+    dict["__float__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyFloat::__float__
+    );
+    dict["__int__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyFloat::__int__
+    );
+    dict["__bool__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyFloat::__bool__
+    );
+}
