4026ed8460b6fe7620f56692b452081084972b92
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-10 09:10:03 +0200

    Implement kleinpy::KPyStr::KPyStr() constructor

---
+++ src/kpystr.cpp
@@ -0,0 +1,39 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+// ---
+KPyStr::KPyStr(std::string txt) : KPyObject(){
+    value = txt;
+
+    dict["__add__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__add__
+    );
+    dict["__float__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__float__
+    );
+    dict["__int__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__int__
+    );
+    dict["__bool__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__bool__
+    );
+    dict["__funlist__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__funlist__
+    );
+    dict["__eq__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__eq__
+    );
+    dict["split"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::split
+    );
+    dict["__getitem__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__getitem__
+    );
+    dict["__len__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__len__
+    );
+    dict["__iter__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyStr::__iter__
+    );
+}
