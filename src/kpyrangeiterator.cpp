24eded9ec4d0389795bb7f6feb9ff6550372791f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-10 08:36:03 +0200

    Implement kleinpy::KPyRangeIterator::KPyRangeIterator() constructor

---
+++ src/kpyrangeiterator.cpp
@@ -0,0 +1,15 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+KPyRangeIterator::KPyRangeIterator(KPyRange *obj) : KPyObject() {
+    rangeObj = obj;
+    index = 0;
+
+    dict["__iter__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyRangeIterator::__iter__
+    );
+    dict["__next__"] = (KPyObject* (KPyObject::*)(std::vector<KPyObject*>*))(
+        &KPyRangeIterator::__next__
+    );
+}
