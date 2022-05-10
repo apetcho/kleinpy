9fc4a087e4b97395f727fd507e3a4bbef4e5ca8f
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-10 09:02:55 +0200

    Implement kleinpy::KPyRangeType::__call__() method

--- src/kpyrangetype.cpp
+++ src/kpyrangetype.cpp
@@ -8,3 +8,70 @@ KPyRangeType::KPyRangeType(std::string typestr, KPyTypeId id)
 
 // ---
 KPyRangeType::~KPyRangeType(){}
+
+// ---
+KPyObject* KPyRangeType::__call__(std::vector<KPyObject*>* args){
+    int start, stop, increment;
+
+    switch(args->size()){
+    case 1:
+        if((*args)[0]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must of int type."
+            );
+        }
+        start = 0;
+        stop = ((KPyInt*)((*args)[0]))->get_value();
+        increment = 1;
+        break;
+    case 2:
+        if((*args)[0]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must be of int type."
+            );
+        }
+        if((*args)[1]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must be of int type."
+            );
+        }
+        start = ((KPyInt*)((*args)[1]))->get_value();
+        stop = ((KPyInt*)((*args)[0]))->get_value();
+        increment = 1;
+        break;
+    case 3:
+        if((*args)[0]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must be of int type."
+            );
+        }
+        if((*args)[1]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must be of int type."
+            );
+        }
+        if((*args)[2]->get_type()->type_id() != KPyTypeId::KPY_INT_TYPE){
+            throw new KPyException(
+                KPYILLEGALOPERATIONEXCEPTION,
+                "range arguments must be of int type."
+            );
+        }
+        start = ((KPyInt*)((*args)[2]))->get_value();
+        stop = ((KPyInt*)((*args)[1]))->get_value();
+        increment = ((KPyInt*)((*args)[0]));
+        break;
+    default:
+        throw new KPyException(
+            KPYILLEGALOPERATIONEXCEPTION,
+            "Incorrect number of arguments for built-in range function."
+        );
+        break;
+    }
+
+    return new KPyRange(start, stop, increment);
+}
