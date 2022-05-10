5319f316edf947ee64036e515ab6bdaec15c184c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-05-10 02:12:47 +0200

    Implement kleinpy::KPyToken::KPyToken() constructor

---
+++ src/kpytoken.cpp
@@ -0,0 +1,11 @@
+#include "kleinpy.hpp"
+
+using namespace kleinpy;
+
+// ---
+KPyToken::KPyToken(int toktype, std::string lex, int lineno, int colno){
+    this->colno = colno;
+    this->lineno = lineno;
+    this->toktype = toktype;
+    this->lex = lex;
+}
